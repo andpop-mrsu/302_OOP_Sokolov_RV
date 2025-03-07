a2550ea5675e3c16d8395f431c59cb2e98ec82d5
Nevall von Goodem <sokoloff.roman.ru@mail.ru>
2025-03-07 19:18:16 +0300

    Task03

---
+++ Task03/run_test.php
@@ -0,0 +1,9 @@
+<?php
+
+    declare(strict_types=1);
+
+    require_once __DIR__ . '/Book.php';
+    require_once __DIR__ . '/BooksList.php';
+    require_once __DIR__ . '/Test.php';
+
+    runTest();
